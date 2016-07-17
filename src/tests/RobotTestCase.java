cf81af0b29cdbbaee33249a05c5d9bf5692521be
Blevenson <blevenson68@gmail.com>
2016-07-17 01:14:57 -0700

    Method for adding timeouts on conditions

--- src/tests/RobotTestCase.java
+++ src/tests/RobotTestCase.java
@@ -2,6 +2,10 @@ package tests;
 import interfaces.ConfigFileReader;
 import interfaces.MyRobot;
 import interfaces.RobotProvider;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeoutException;
+
 import junit.framework.TestCase;
 
 public class RobotTestCase extends TestCase{
@@ -22,5 +26,21 @@ public class RobotTestCase extends TestCase{
 			e.printStackTrace();
 		}
 		robot.robotInit();
+	}
+	
+	/**
+	 * Wait for condition or until it timesout
+	 * 
+	 * @param timeout Seconds
+	 * @param condition
+	 * @throws Exception
+	 */
+	public void wait(double timeout, Callable<Boolean> condition) throws Exception{
+		double endTime = System.currentTimeMillis() + (timeout * 1000);
+		while(endTime > System.currentTimeMillis()){
+			if(condition.call())
+				return;
+		}
+		throw new TimeoutException("Waited too long");
 	}
 }
